47df0362e15f22c5f2c4ed00170cd3bd7a905306
Dilawar-Shehzad <idilawarshehzad@gmail0.com>
2023-10-12 20:33:19 +0500

    First commit

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const tape = require('tape')
 const jsonist = require('jsonist')
 
 const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
-const server = require('./server')
+const server = requre('./server')
 
 const urlBase = `http://localhost:${PORT}`
 
